2b72d3c8c7568290fd975094b617699b682a6b95
Jose Chong <127264125+Jlchong3@users.noreply.github.com>
2025-09-06 03:52:58 -0500

    Changes to storage api and added annotations

--- lua/storage.lua
+++ lua/storage.lua
@@ -1,3 +1,7 @@
+---@class Storage
+---@field data_path string
+---@field save fun(tbl:table)
+---@field load fun(): table|nil
 local M = {}
 
 ---@param path string
@@ -30,15 +34,13 @@ local create_file_path = function(basedir, filename)
     return string.format('%s/%s', basedir, filename)
 end
 
-M.data_path = string.format('%s/drawer', vim.fn.stdpath('data'))
-
 ---@param filename string
-M.get_data_file_name = function(filename)
+local get_data_file_name = function(filename)
     return hash(filename) .. '.lua'
 end
 
 ---@param tbl table
-M.serialize = function (tbl)
+local function serialize(tbl)
     local result = '{'
     for k, v in pairs(tbl) do
         local key
@@ -50,7 +52,7 @@ M.serialize = function (tbl)
 
         local value
         if type(v) == 'table' then
-            value = M.serialize(v)
+            value = serialize(v)
         elseif type(v) == 'string' then
             value = string.format('%q', v)
         else
@@ -64,23 +66,26 @@ M.serialize = function (tbl)
     return result
 end
 
+
+M.data_path = string.format('%s/drawer', vim.fn.stdpath('data'))
+
 ---@param tbl table
-M.save_data = function(tbl)
+M.save = function(tbl)
     ensure_dir(M.data_path)
 
-    local filename = M.get_data_file_name(vim.fn.getcwd()) -- filename is based on cwd
+    local filename = get_data_file_name(vim.fn.getcwd()) -- filename is based on cwd
     local data_file_path = create_file_path(M.data_path, filename)
 
     if not file_exists(data_file_path) and #tbl == 0 then return end
 
     local f = assert(io.open(data_file_path, 'w'))
 
-    f:write(M.serialize(tbl))
+    f:write(serialize(tbl))
     f:close()
 end
 
-M.load_table = function ()
-    local filename = M.get_data_file_name(vim.fn.getcwd())
+M.load = function ()
+    local filename = get_data_file_name(vim.fn.getcwd())
     local data_file_path = create_file_path(M.data_path, filename)
 
     local f = io.open(data_file_path)
@@ -92,3 +97,5 @@ M.load_table = function ()
     local chunk = assert(load(content))
     return chunk()
 end
+
+return M
